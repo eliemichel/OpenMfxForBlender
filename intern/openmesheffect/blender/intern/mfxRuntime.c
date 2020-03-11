6463fbd60eb973ac9a59609253d290f75e69893e
EitanSomething <eitant13@gmail.com>
2020-03-10 20:52:49 -0400

    clang-format

--- intern/openmesheffect/blender/intern/mfxRuntime.c
+++ intern/openmesheffect/blender/intern/mfxRuntime.c
@@ -25,19 +25,20 @@
 #include "mfxCallbacks.h"
 #include "mfxRuntime.h"
 
-#include "DNA_mesh_types.h" // Mesh
-#include "DNA_meshdata_types.h" // MVert
+#include "DNA_mesh_types.h"      // Mesh
+#include "DNA_meshdata_types.h"  // MVert
 
-#include "BKE_mesh.h" // BKE_mesh_new_nomain
-#include "BKE_main.h" // BKE_main_blendfile_path_from_global
+#include "BKE_mesh.h"  // BKE_mesh_new_nomain
+#include "BKE_main.h"  // BKE_main_blendfile_path_from_global
 
 #include "BLI_math_vector.h"
 #include "BLI_string.h"
 #include "BLI_path_util.h"
 
- // Private
+// Private
 
-void runtime_free_effect_instance(OpenMeshEffectRuntime *rd) {
+void runtime_free_effect_instance(OpenMeshEffectRuntime *rd)
+{
   if (rd->is_plugin_valid) {
     OfxPlugin *plugin = rd->registry.plugins[rd->effect_index];
     OfxPluginStatus status = rd->registry.status[rd->effect_index];
@@ -57,19 +58,24 @@ void runtime_free_effect_instance(OpenMeshEffectRuntime *rd) {
   }
 }
 
-void runtime_ensure_host(OpenMeshEffectRuntime *rd) {
+void runtime_ensure_host(OpenMeshEffectRuntime *rd)
+{
   if (NULL == rd->ofx_host) {
     rd->ofx_host = getGlobalHost();
 
     // Configure host
-    OfxPropertySuiteV1 *propertySuite = (OfxPropertySuiteV1*)rd->ofx_host->fetchSuite(rd->ofx_host->host, kOfxPropertySuite, 1);
+    OfxPropertySuiteV1 *propertySuite = (OfxPropertySuiteV1 *)rd->ofx_host->fetchSuite(
+        rd->ofx_host->host, kOfxPropertySuite, 1);
     // Set custom callbacks
-    propertySuite->propSetPointer(rd->ofx_host->host, kOfxHostPropBeforeMeshGetCb, 0, (void*)before_mesh_get);
-    propertySuite->propSetPointer(rd->ofx_host->host, kOfxHostPropBeforeMeshReleaseCb, 0, (void*)before_mesh_release);
+    propertySuite->propSetPointer(
+        rd->ofx_host->host, kOfxHostPropBeforeMeshGetCb, 0, (void *)before_mesh_get);
+    propertySuite->propSetPointer(
+        rd->ofx_host->host, kOfxHostPropBeforeMeshReleaseCb, 0, (void *)before_mesh_release);
   }
 }
 
-bool runtime_ensure_effect_instance(OpenMeshEffectRuntime *rd) {
+bool runtime_ensure_effect_instance(OpenMeshEffectRuntime *rd)
+{
 
   if (false == rd->is_plugin_valid) {
     return false;
@@ -90,7 +96,8 @@ bool runtime_ensure_effect_instance(OpenMeshEffectRuntime *rd) {
     if (OfxPluginStatNotLoaded == *pStatus) {
       if (ofxhost_load_plugin(rd->ofx_host, plugin)) {
         *pStatus = OfxPluginStatOK;
-      } else {
+      }
+      else {
         *pStatus = OfxPluginStatError;
         return false;
       }
@@ -106,7 +113,8 @@ bool runtime_ensure_effect_instance(OpenMeshEffectRuntime *rd) {
   return true;
 }
 
-void runtime_reset_plugin_path(OpenMeshEffectRuntime *rd) {
+void runtime_reset_plugin_path(OpenMeshEffectRuntime *rd)
+{
   if (rd->is_plugin_valid) {
     if (-1 != rd->effect_index) {
       runtime_free_effect_instance(rd);
@@ -124,7 +132,8 @@ void runtime_reset_plugin_path(OpenMeshEffectRuntime *rd) {
 
 // Public
 
-void runtime_init(OpenMeshEffectRuntime *rd) {
+void runtime_init(OpenMeshEffectRuntime *rd)
+{
   rd->plugin_path[0] = '\0';
   rd->is_plugin_valid = false;
   rd->effect_index = 0;
@@ -133,7 +142,8 @@ void runtime_init(OpenMeshEffectRuntime *rd) {
   rd->effect_instance = NULL;
 }
 
-void runtime_free(OpenMeshEffectRuntime *rd) {
+void runtime_free(OpenMeshEffectRuntime *rd)
+{
   runtime_reset_plugin_path(rd);
 
   if (NULL != rd->ofx_host) {
@@ -142,7 +152,8 @@ void runtime_free(OpenMeshEffectRuntime *rd) {
   }
 }
 
-void runtime_set_plugin_path(OpenMeshEffectRuntime *rd, const char *plugin_path) {
+void runtime_set_plugin_path(OpenMeshEffectRuntime *rd, const char *plugin_path)
+{
   if (0 == strcmp(rd->plugin_path, plugin_path)) {
     return;
   }
@@ -159,7 +170,9 @@ void runtime_set_plugin_path(OpenMeshEffectRuntime *rd, const char *plugin_path)
   // Get absolute path (ui file browser returns relative path for saved files)
   char abs_path[FILE_MAX];
   BLI_strncpy(abs_path, rd->plugin_path, FILE_MAX);
-  const char *base_path = BKE_main_blendfile_path_from_global(); // TODO: How to get a bMain object here to avoid "from_global()"?
+  const char *base_path =
+      BKE_main_blendfile_path_from_global();  // TODO: How to get a bMain object here to avoid
+                                              // "from_global()"?
   if (NULL != base_path) {
     BLI_path_abs(abs_path, base_path);
   }
@@ -171,18 +184,20 @@ void runtime_set_plugin_path(OpenMeshEffectRuntime *rd, const char *plugin_path)
   rd->is_plugin_valid = true;
 }
 
-void runtime_set_effect_index(OpenMeshEffectRuntime *rd, int effect_index) {
+void runtime_set_effect_index(OpenMeshEffectRuntime *rd, int effect_index)
+{
   if (rd->effect_index == effect_index) {
     return;
   }
-  
+
   if (-1 != rd->effect_index) {
     runtime_free_effect_instance(rd);
   }
 
   if (rd->is_plugin_valid) {
     rd->effect_index = min(max(-1, effect_index), rd->registry.num_plugins - 1);
-  } else {
+  }
+  else {
     rd->effect_index = -1;
   }
 
@@ -191,41 +206,46 @@ void runtime_set_effect_index(OpenMeshEffectRuntime *rd, int effect_index) {
   }
 }
 
-void runtime_get_parameters_from_rna(OpenMeshEffectRuntime *rd, OpenMeshEffectModifierData *fxmd) {
+void runtime_get_parameters_from_rna(OpenMeshEffectRuntime *rd, OpenMeshEffectModifierData *fxmd)
+{
   OfxParamHandle *parameters = rd->effect_instance->parameters.parameters;
-  for (int i = 0 ; i < fxmd->num_parameters ; ++i) {
+  for (int i = 0; i < fxmd->num_parameters; ++i) {
     switch (parameters[i]->type) {
-    case PARAM_TYPE_INTEGER_3D:
-      parameters[i]->value[2].as_int = fxmd->parameter_info[i].integer_vec_value[2];
-    case PARAM_TYPE_INTEGER_2D:
-      parameters[i]->value[1].as_int = fxmd->parameter_info[i].integer_vec_value[1];
-    case PARAM_TYPE_INTEGER:
-      parameters[i]->value[0].as_int = fxmd->parameter_info[i].integer_vec_value[0];
-      break;
-    
-    case PARAM_TYPE_RGBA:
-      parameters[i]->value[3].as_double = (double)fxmd->parameter_info[i].float_vec_value[3];
-    case PARAM_TYPE_DOUBLE_3D:
-    case PARAM_TYPE_RGB:
-      parameters[i]->value[2].as_double = (double)fxmd->parameter_info[i].float_vec_value[2];
-    case PARAM_TYPE_DOUBLE_2D:
-      parameters[i]->value[1].as_double = (double)fxmd->parameter_info[i].float_vec_value[1];
-    case PARAM_TYPE_DOUBLE:
-      parameters[i]->value[0].as_double = (double)fxmd->parameter_info[i].float_vec_value[0];
-      break;
+      case PARAM_TYPE_INTEGER_3D:
+        parameters[i]->value[2].as_int = fxmd->parameter_info[i].integer_vec_value[2];
+      case PARAM_TYPE_INTEGER_2D:
+        parameters[i]->value[1].as_int = fxmd->parameter_info[i].integer_vec_value[1];
+      case PARAM_TYPE_INTEGER:
+        parameters[i]->value[0].as_int = fxmd->parameter_info[i].integer_vec_value[0];
+        break;
 
-    case PARAM_TYPE_BOOLEAN:
-      parameters[i]->value[0].as_bool = fxmd->parameter_info[i].integer_vec_value[0];
-      break;
+      case PARAM_TYPE_RGBA:
+        parameters[i]->value[3].as_double = (double)fxmd->parameter_info[i].float_vec_value[3];
+      case PARAM_TYPE_DOUBLE_3D:
+      case PARAM_TYPE_RGB:
+        parameters[i]->value[2].as_double = (double)fxmd->parameter_info[i].float_vec_value[2];
+      case PARAM_TYPE_DOUBLE_2D:
+        parameters[i]->value[1].as_double = (double)fxmd->parameter_info[i].float_vec_value[1];
+      case PARAM_TYPE_DOUBLE:
+        parameters[i]->value[0].as_double = (double)fxmd->parameter_info[i].float_vec_value[0];
+        break;
 
-    case PARAM_TYPE_STRING:
-      parameter_realloc_string(parameters[i], MOD_OPENMESHEFFECT_MAX_STRING_VALUE);
-      strncpy(parameters[i]->value[0].as_char, fxmd->parameter_info[i].string_value, MOD_OPENMESHEFFECT_MAX_STRING_VALUE);
-      break;
+      case PARAM_TYPE_BOOLEAN:
+        parameters[i]->value[0].as_bool = fxmd->parameter_info[i].integer_vec_value[0];
+        break;
 
-    default:
-      printf("-- Skipping parameter %s (unsupported type: %d)\n", parameters[i]->name, parameters[i]->type);
-      break;
+      case PARAM_TYPE_STRING:
+        parameter_realloc_string(parameters[i], MOD_OPENMESHEFFECT_MAX_STRING_VALUE);
+        strncpy(parameters[i]->value[0].as_char,
+                fxmd->parameter_info[i].string_value,
+                MOD_OPENMESHEFFECT_MAX_STRING_VALUE);
+        break;
+
+      default:
+        printf("-- Skipping parameter %s (unsupported type: %d)\n",
+               parameters[i]->name,
+               parameters[i]->type);
+        break;
     }
   }
 }
